4c2391696cfeffe9577f83fb00a15b9ff8dce4a3
kidatom <theo@chemel.net>
2018-03-27 10:03:59 -0400

    started class definitions

---
+++ snowDay/Forecast.swift
@@ -0,0 +1,17 @@
+//
+//  Forecast.swift
+//  snowDay
+//
+//  Created by Theo Chemel on 3/26/18.
+//  Copyright © 2018 Theo Chemel. All rights reserved.
+//
+
+import Foundation
+
+class Forecast {
+    var weatherConditions: [String:Any]
+    var date: Date
+    var predictionPercent: Float
+    var predictionBool: Bool
+        
+}
---
+++ snowDay/PullDarkSkyForecast.swift
@@ -0,0 +1,16 @@
+//
+//  PullDarkSkyForecast.swift
+//  snowDay
+//
+//  Created by Theo Chemel on 3/26/18.
+//  Copyright © 2018 Theo Chemel. All rights reserved.
+//
+
+import Foundation
+
+func pullDarkSkyForecast(location: (Float, Float)) -> Forecast {
+    
+    let forecast: Forecast!
+    forecast.date = Date.init()
+    return forecast
+}
